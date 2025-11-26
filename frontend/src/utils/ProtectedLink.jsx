325b48825f7ce09159df37b93565361ce3288f80
Ansh <anshmalhotra9718116717@gmail.com>
2025-11-27 02:45:44 +0530

    first commit 	new file:   Backend/.env 	new file:   Backend/.gitignore 	new file:   Backend/Controller/userController.js 	new file:   Backend/Models/user.js 	new file:   Backend/app.js 	new file:   Backend/package-lock.json 	new file:   Backend/package.json 	new file:   Backend/routes/userRoutes.js 	new file:   Backend/utils/auth.js 	new file:   frontend/.env 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/eslint.config.js 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.css 	new file:   frontend/src/App.jsx 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/components/ConsoleComponent.jsx 	new file:   frontend/src/components/ContainerLogin.jsx 	new file:   frontend/src/components/ContainerRegister.jsx 	new file:   frontend/src/css/Container.css 	new file:   frontend/src/css/Game.css 	new file:   frontend/src/main.jsx 	new file:   frontend/src/pages/Console.jsx 	new file:   frontend/src/pages/Game.jsx 	new file:   frontend/src/pages/Login.jsx 	new file:   frontend/src/pages/Register.jsx 	new file:   frontend/src/utils/ProtectedLink.jsx 	new file:   frontend/vite.config.js

---
+++ frontend/src/utils/ProtectedLink.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
+
+export default function ProtectedRoute({ children }) {
+    const token = localStorage.getItem("token");
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_HOST}/user/verifyToken`,{
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then((res)=>{
+        localStorage.setItem("username", res.data.username);
+        console.log("Token is valid");
+    }).catch((err)=>{
+        token = undefined;
+        console.log("Token is invalid");
+        return (<Navigate to="/" replace />);
+    });
+    }, []);
+
+    if (!token) {
+        return (<Navigate to="/" replace />);
+    }
+
+    return children;
+}
